ad919d49545050008bb9532f36206ddeb1e98c46
Md.Nazmul Hasan <028194@jb.com>
2022-04-07 15:29:06 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import firebaseConfig from './firebase.config';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div >
+
+        <Button variant="primary">Primary</Button>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDqo4ATf56lFuXNdKikQvoZ4BuL6l3OyzM",
+    authDomain: "burj-khalifa-fcdd7.firebaseapp.com",
+    projectId: "burj-khalifa-fcdd7",
+    storageBucket: "burj-khalifa-fcdd7.appspot.com",
+    messagingSenderId: "882790533023",
+    appId: "1:882790533023:web:fdec5ff0e1eb69ba7c5faa"
+  };
+  export default firebaseConfig;
